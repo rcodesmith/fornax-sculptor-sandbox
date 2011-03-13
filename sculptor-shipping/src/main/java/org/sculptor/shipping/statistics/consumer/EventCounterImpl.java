521e0aea15093feaae3492b3ff11ab008ae0ae67
patrik_nordwall <patrik_nordwall@df330e69-2214-0410-8926-af1636e6a837>
2011-03-13 21:05:42 +0000

    Release tag for version 2.0.0
    git-svn-id: https://fornax.svn.sourceforge.net/svnroot/fornax/tags/cartridges/sculptor/2.0.0@7569 df330e69-2214-0410-8926-af1636e6a837

---
+++ sculptor-shipping/src/main/java/org/sculptor/shipping/statistics/consumer/EventCounterImpl.java
@@ -0,0 +1,25 @@
+package org.sculptor.shipping.statistics.consumer;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.fornax.cartridges.sculptor.framework.event.Event;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of EventCounter.
+ */
+@Component("eventCounter")
+public class EventCounterImpl extends EventCounterImplBase {
+    private final Log log = LogFactory.getLog(getClass());
+    private final AtomicInteger counter = new AtomicInteger();
+
+    public EventCounterImpl() {
+    }
+
+    public void receive(Event event) {
+        int current = counter.incrementAndGet();
+        log.info(String.format("Number of events: %s", current));
+    }
+}
