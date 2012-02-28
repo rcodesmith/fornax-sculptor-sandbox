3dc75e527cd13a331330f0511096ab2b91fab021
Ron Smith <ron@rpstechnologies.net>
2012-02-28 00:16:04 -0600

    Support for place named parameters

--- fornax-cartridges-sculptor-gwt-framework/src/main/java/org/fornax/cartridges/sculptor/framework/gwt/client/util/GwtConvertUtils.java
+++ fornax-cartridges-sculptor-gwt-framework/src/main/java/org/fornax/cartridges/sculptor/framework/gwt/client/util/GwtConvertUtils.java
@@ -5,4 +5,12 @@ public class GwtConvertUtils {
 	public static Long convert(String from, Long dummyTo) {
 		return (from == null? null : Long.parseLong(from));
 	}
+	
+	public static String toString(Long val) {
+		return (val == null? null : val.toString());
+	}
+	
+	public static String toString(Boolean val) {
+		return (val == null? null : val.toString());		
+	}
 }
