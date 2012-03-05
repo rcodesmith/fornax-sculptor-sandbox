a3822b57bd15fddddb191fe51e445f3ef6599c0a
Ron Smith <ron@rpstechnologies.net>
2012-03-05 14:30:33 -0600

    Cleanup GwtPlaceTemplateTest

--- fornax-cartridges-sculptor-gwt/src/test/java/org/fornax/cartridges/sculptor/generator/templates/GwtPlaceTemplateTest.java
+++ fornax-cartridges-sculptor-gwt/src/test/java/org/fornax/cartridges/sculptor/generator/templates/GwtPlaceTemplateTest.java
@@ -14,27 +14,6 @@ import sculptorguimetamodel.GuiModule;
 import sculptorguimetamodel.View;
 
 public class GwtPlaceTemplateTest extends TemplateTestBase {
-//	private static GuiApplication guiApp;
-
-//	@BeforeClass
-//	public static void before() throws Exception {
-//		System.setProperty("project.nature", "business-tier, rcp");
-//		System.setProperty("datetime.library", "joda");
-//		System.setProperty("gui.createDefaults", "false");
-//		System.setProperty("package.gwt", "gwt");
-//
-//		initWorkflowContext("workflowguidsl-test-library-gwt.mwe");
-//		guiApp = (GuiApplication) ctx.get("guiModel");
-//	}
-//
-//	@AfterClass
-//	public static void after() {
-//		System.getProperties().remove("project.nature");
-//		System.getProperties().remove("datetime.library");
-//		System.getProperties().remove("gui.createDefaults");
-//		System.getProperties().remove("package.gwt");
-//
-//	}
 
 	private GuiModule personModule() {
 		return (GuiModule) getNamedElement("person", guiApp.getModules());
@@ -67,15 +46,6 @@ public class GwtPlaceTemplateTest extends TemplateTestBase {
 				"public PersonFormPlace(", "Long personId",
 				") {");
 
-		//
-		// assertContains(activityBaseCode,
-		// "private final org.fornax.cartridges.sculptor.examples.library.person.gwt.client.view.TableViewView view;");
-		//
-		// assertContains(activityBaseCode,
-		// "protected abstract void populateFromPlace();");
-		//
-		// assertContains(activityBaseCode,
-		// "protected com.google.gwt.user.client.ui.Widget getViewAsWidget() {");
 	}
 
 }
