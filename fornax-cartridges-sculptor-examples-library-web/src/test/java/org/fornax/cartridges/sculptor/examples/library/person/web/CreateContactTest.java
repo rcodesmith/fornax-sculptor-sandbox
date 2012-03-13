ddc00602e110fbd4f534432fc97146ee31e56d54
Ron Smith <ron@rpstechnologies.net>
2012-03-12 20:53:22 -0500

    add examples-library-web and helloworld-web sample project files that are generated only once

---
+++ fornax-cartridges-sculptor-examples-library-web/src/test/java/org/fornax/cartridges/sculptor/examples/library/person/web/CreateContactTest.java
@@ -0,0 +1,21 @@
+package org.fornax.cartridges.sculptor.examples.library.person.web;
+
+public class CreateContactTest extends CreateContactTestBase {
+
+    /**
+     * Populates the valid form values.
+     */
+    protected void populateFormSuccess(CreateContactForm form) {
+
+        // TODO Auto-generated method stub, remove next line and assign valid form values
+    }
+
+    /**
+     * Populates the invalid form values. It is possible to throw
+     * UnsupportedOperationException to skip testFormSubmitError.
+     */
+    protected void populateFormError(CreateContactForm form) {
+
+        // TODO Auto-generated method stub, remove next line and assign some invalid form values
+    }
+}
---
+++ fornax-cartridges-sculptor-examples-library-web/src/test/java/org/fornax/cartridges/sculptor/examples/library/person/web/UpdateContactTest.java
@@ -0,0 +1,21 @@
+package org.fornax.cartridges.sculptor.examples.library.person.web;
+
+public class UpdateContactTest extends UpdateContactTestBase {
+
+    /**
+     * Populates the valid form values.
+     */
+    protected void populateFormSuccess(UpdateContactForm form) {
+
+        // TODO Auto-generated method stub, remove next line and assign valid form values
+    }
+
+    /**
+     * Populates the invalid form values. It is possible to throw
+     * UnsupportedOperationException to skip testFormSubmitError.
+     */
+    protected void populateFormError(UpdateContactForm form) {
+
+        // TODO Auto-generated method stub, remove next line and assign some invalid form values
+    }
+}
