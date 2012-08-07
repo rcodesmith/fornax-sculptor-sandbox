bf70628225cf6462ba44fb44c1044400b13851a2
Ron Smith <ron@rpstechnologies.net>
2012-08-07 07:53:42 -0500

    Support for cell table onClick binding for button widgets. Maven table column templates into their own file.

--- fornax-cartridges-sculptor-gwt-framework/src/main/java/org/fornax/cartridges/sculptor/framework/gwt/client/behavior/EventFactory.java
+++ fornax-cartridges-sculptor-gwt-framework/src/main/java/org/fornax/cartridges/sculptor/framework/gwt/client/behavior/EventFactory.java
@@ -16,11 +16,28 @@ public class EventFactory {
 		return ev;
 	}
 	
+	/**
+	 * Create Event for a table cell
+	 * @param obj - Object corresponding to a row in a table
+	 * @param val - Value corresponding to a column
+	 * @return
+	 */
 	public static BehaviorDispatcher.Event create(Object obj, Object val) {
 		Event ev = new Event(obj, val);
 		return ev;
 	}
-	
+
+	/**
+	 * Create Event for a table cell where a cell value doesn't exist or isn't relevant (e.g. a click on a cell)
+	 * @param obj - Object corresponding to a row in a table
+	 * @param val - Value corresponding to a column
+	 * @return
+	 */
+	public static BehaviorDispatcher.Event create(Object obj) {
+		Event ev = new Event(obj, null);
+		return ev;
+	}
+
 	public static BehaviorDispatcher.Event create(com.google.gwt.event.logical.shared.SelectionEvent<com.google.gwt.user.client.ui.SuggestOracle.Suggestion> event) {
 		Event ev = new Event();
 		return ev;
